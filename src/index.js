92542f165938df15d638f33992e74fe43ab41f87
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2022-08-25 00:53:36 +0100

    Introduced ES6 modules

--- src/index.js
+++ src/index.js
@@ -1,32 +1,7 @@
 import './style.css';
+import data from "./modules/data";
 
-const data = [
-  {
-    id: 0,
-    name: 'John Doe',
-    score: '100',
-  },
-  {
-    id: 1,
-    name: 'Peter Parker',
-    score: '50',
-  },
-  {
-    id: 2,
-    name: 'Frank Castle',
-    score: '75',
-  },
-  {
-    id: 3,
-    name: 'Promise Uka',
-    score: '65',
-  },
-  {
-    id: 4,
-    name: 'Evy Suno',
-    score: '90',
-  },
-];
+
 document.getElementById('score-table').innerHTML = data
   .map(
     (item) => `

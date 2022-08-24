04efc396037c092f3783410223cc033717468078
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2022-08-25 00:30:12 +0100

    Add linters and fix all linter errors

--- src/index.js
+++ src/index.js
@@ -1,35 +1,35 @@
-import "./style.css";
+import './style.css';
 
 const data = [
   {
     id: 0,
-    name: "John Doe",
-    score: "100",
+    name: 'John Doe',
+    score: '100',
   },
   {
     id: 1,
-    name: "Peter Parker",
-    score: "50",
+    name: 'Peter Parker',
+    score: '50',
   },
   {
     id: 2,
-    name: "Frank Castle",
-    score: "75",
+    name: 'Frank Castle',
+    score: '75',
   },
   {
     id: 3,
-    name: "Promise Uka",
-    score: "65",
+    name: 'Promise Uka',
+    score: '65',
   },
   {
     id: 4,
-    name: "Evy Suno",
-    score: "90",
+    name: 'Evy Suno',
+    score: '90',
   },
 ];
-document.getElementById("score-table").innerHTML = data
+document.getElementById('score-table').innerHTML = data
   .map(
     (item) => `
-<p class="score">${item.name}: ${item.score}</p>`
+<p class="score">${item.name}: ${item.score}</p>`,
   )
-  .join(" ");
+  .join(' ');

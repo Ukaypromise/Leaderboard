f279bab5512952b28cd5d79e7a02140b95674684
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2022-08-25 13:28:17 +0100

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,24 +1,21 @@
-import "./style.css";
-import UI from "./modules/renderItems.js";
-import id from "./modules/gameId.js";
+import './style.css';
+import UI from './modules/renderItems.js';
+import id from './modules/gameId.js';
 
-const url =
-  "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games";
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games';
 
-  
-
-const submit = document.querySelector(".submitBtn");
-const refreshBtn = document.querySelector("#refresh-btn");
-const user = document.getElementById("name");
-const score = document.getElementById("score");
+const submit = document.querySelector('.submitBtn');
+const refreshBtn = document.querySelector('#refresh-btn');
+const user = document.getElementById('name');
+const score = document.getElementById('score');
 
 UI.getData();
 
 const prevScores = async () => {
   const res = await fetch(`${url}/${id}/scores`, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-type": "application/json; charset=UTF-8",
+      'Content-type': 'application/json; charset=UTF-8',
     },
     body: JSON.stringify({
       user: user.value,
@@ -30,16 +27,16 @@ const prevScores = async () => {
 };
 
 const clearInput = () => {
-  user.value = "";
-  score.value = "";
+  user.value = '';
+  score.value = '';
 };
 
-submit.addEventListener("click", (e) => {
+submit.addEventListener('click', (e) => {
   e.preventDefault();
   prevScores();
   clearInput();
 });
 
-refreshBtn.addEventListener("click", () => {
+refreshBtn.addEventListener('click', () => {
   window.location.reload();
 });
